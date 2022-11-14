3bd8ee617a0f9baca701b0e6654e37f87eb27edd
Mishnieva Julia <juli8md@gmal.com>
2022-11-14 17:35:46 +0100

    added change

--- Main/Java/com/hillel/Mishnieva/lessons1/Main.java
+++ Main/Java/com/hillel/Mishnieva/lessons1/Main.java
@@ -3,5 +3,6 @@ package com.hillel.Mishnieva.lessons1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        System.out.println("Hi");
     }
 }
--- Main/Java/com/hillel/Mishnieva/lessons2/Main.java
+++ Main/Java/com/hillel/Mishnieva/lessons2/Main.java
@@ -6,6 +6,15 @@ public class Main {
         byte age;
         age = 28;
 
+        int a = 15;
+        int b = 5;
+
+        int result = (a + b)/4;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println("result = " + result);
         System.out.println(age);
     }
 }
