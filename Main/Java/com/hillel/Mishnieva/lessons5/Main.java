af494dd21a57ce81a057b69cc3ff471a38800f86
Mishnieva Julia <juli8md@gmal.com>
2022-11-29 12:55:48 +0100

    Counter Strike

---
+++ Main/Java/com/hillel/Mishnieva/lessons5/Main.java
@@ -0,0 +1,35 @@
+package com.hillel.Mishnieva.lessons5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Bведіть ім'я першої команди");
+        String teamName1 = scanner1.nextLine();
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Bведіть кількість фрагів");
+        int team1Player1 = scanner2.nextInt();
+        int team1Player2 = scanner2.nextInt();
+        int team1Player3 = scanner2.nextInt();
+        int team1Player4 = scanner2.nextInt();
+        int team1Player5 = scanner2.nextInt();
+        System.out.println("Bведіть ім'я другої команди");
+        String teamName2 = scanner1.nextLine();
+        System.out.println("Bведіть кількість фрагів");
+        int team2Player1 = scanner2.nextInt();
+        int team2Player2 = scanner2.nextInt();
+        int team2Player3 = scanner2.nextInt();
+        int team2Player4 = scanner2.nextInt();
+        int team2Player5 = scanner2.nextInt();
+
+        int result1 = (team1Player1 + team1Player2 + team1Player3 + team1Player4 + team1Player5)/5;
+        int result2 = (team2Player1 + team2Player2 + team2Player3 + team2Player4 + team2Player5)/5;
+
+        if (result1 > result2) {
+            System.out.println("Перемогла команда " + teamName1 + " набрала " + result1 + " очків");
+        } else {
+            System.out.println("Перемогла команда " + teamName2 + " набрала " + result2 + " очків");
+        }
+    }
+}
